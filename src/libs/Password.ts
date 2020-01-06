ce368c31051712afc31070a6cb9897058882e64e
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-01-06 18:33:05 -0300

    refactor: remove argon2 do password e deixa bcrypt padrão

--- src/libs/Password.ts
+++ src/libs/Password.ts
@@ -1,33 +1,39 @@
 import bcrypt from 'bcryptjs';
-import argon2 from 'argon2';
+// import argon2 from 'argon2';
 
-export interface IPasswordResponse {
-  create: (value: string) => Promise<string>;
-  verify: (value: string, hashed: string) => Promise<boolean>;
-}
+// export interface IPasswordResponse {
+//   create: (value: string) => Promise<string>;
+//   verify: (value: string, hashed: string) => Promise<boolean>;
+// }
 
-class Password {
-  public get bcrypt(): IPasswordResponse {
-    return {
-      create(value: string): Promise<string> {
-        return bcrypt.hash(value, 12);
-      },
-      verify(value: string, hashed: string): Promise<boolean> {
-        return bcrypt.compare(value, hashed);
-      },
-    };
+export default class Password {
+  static create(value: string): Promise<string> {
+    return bcrypt.hash(value, 12);
   }
 
-  public get argon2(): IPasswordResponse {
-    return {
-      create(value: string): Promise<string> {
-        return argon2.hash(value);
-      },
-      verify(value: string, hashed: string): Promise<boolean> {
-        return argon2.verify(hashed, value);
-      },
-    };
+  static verify(value: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(value, hashed);
   }
-}
 
-export default new Password();
+  // public get bcrypt(): IPasswordResponse {
+  //   return {
+  //     create(value: string): Promise<string> {
+  //       return bcrypt.hash(value, 12);
+  //     },
+  //     verify(value: string, hashed: string): Promise<boolean> {
+  //       return bcrypt.compare(value, hashed);
+  //     },
+  //   };
+  // }
+
+  // public get argon2(): IPasswordResponse {
+  //   return {
+  //     create(value: string): Promise<string> {
+  //       return argon2.hash(value);
+  //     },
+  //     verify(value: string, hashed: string): Promise<boolean> {
+  //       return argon2.verify(hashed, value);
+  //     },
+  //   };
+  // }
+}
